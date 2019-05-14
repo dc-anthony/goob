f4515232d3f86319f76ba82debcc68d7860a29fd
DC Anthony <dolan.c.anthony@gmail.com>
2019-05-14 13:00:28 -0400

    • implemented starting structure and css • commented out foundation

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
-}
+  return (
+    <div className="app-component">
+      <h1>goob</h1>
+      <div className="main-subtitle">a discord chat bot embed</div>
+      <div className="example">EXAMPLE</div>
+      <div className="chat-embed">CHAT EMBED HERE</div>
+    </div>
+  );
+};
 
-export default App
+export default App;
