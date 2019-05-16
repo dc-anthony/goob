9571bebe037f0196862ff591754c32b9f2b38768
DC Anthony <dolan.c.anthony@gmail.com>
2019-05-15 21:09:39 -0400

    • commented back in stuff in chat_channel.rb

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -43,12 +43,10 @@ class ChatChannel < ApplicationCable::Channel
   end
 
   goob_bot.message do |event|
-    binding.pry
-    new_server = Server.new
     # new_server.server = event.server
-    new_server.server_name = event.server.name
-    new_server.discord_server_id = event.server.id
-    new_server.owner = event.server.owner
+    server_name = event.server.name
+    discord_server_id = event.server.id
+    owner = event.server.owner
     # server_channels = event.server.channels
     # channel = event.channel
     channel_name = event.channel.name
@@ -62,9 +60,9 @@ class ChatChannel < ApplicationCable::Channel
     new_message.message_author = event.message.author
     new_message.message_channel = event.message.channel
 
-    if new_message.save && new_channel.save && new_server.save
-      #do some action cable stuff here
-    end
+    # if new_message.save && new_channel.save && new_server.save
+    #   #do some action cable stuff here
+    # end
   end
 
   goob_bot.run
