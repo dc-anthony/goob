51ae8a51db9e1df60a9db413b2f88fe543277136
DC Anthony <dolan.c.anthony@gmail.com>
2019-05-17 09:37:57 -0400

    • deleted servers controller

--- app/controllers/api/v1/servers_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::ServersController < ApplicationController
-  protect_from_forgery unless: -> { request.format.json? }
-
-  def index
-    servers = Server.all
-    render json: servers
-  end
-end
