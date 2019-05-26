129e9990a2c45c9f3360e4dc15d264acb362e0a1
DC Anthony <dolan.c.anthony@gmail.com>
2019-05-25 20:02:47 -0400

    • updated factory_bot.rb config for rails

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,5 +1,9 @@
 require 'factory_bot_rails'
 
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
+
 FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
