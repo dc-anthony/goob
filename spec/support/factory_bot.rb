f66894fc14039543e7d645baab05cb9dc82396a4
DC Anthony <dolan.c.anthony@gmail.com>
2019-05-25 20:06:03 -0400

    • deleted repeated user factory in factory_bot.rb

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,12 +3,3 @@ require 'factory_bot_rails'
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
-
-FactoryBot.define do
-  factory :user do
-    sequence(:email) {|n| "user#{n}@example.com" }
-    password { 'password' }
-    password_confirmation { 'password' }
-    role { "guest" }
-  end
-end
